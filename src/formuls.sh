1c8bfe2a315ec505b2d77674a52ab2296de92f9f
jrdooley <info@formuls.co.uk>
2022-04-05 14:12:06 +0200

    Major updates-again!
    pd
    - refactored code:
    — routing is embedded in f.util.oscparse abstraction and related objects
    — added automation etc to rolling freezer
    — added hold and random function to repeater fx
    — Updated f.util.keyselector
    — added f.util.delbang abstraction
    — added f.gen.weighted abstraction
    
    o-s-c
    - updated interface for added functionality
    
    Signed-off-by: jrdooley <info@formuls.co.uk>

--- src/formuls.sh
+++ src/formuls.sh
@@ -5,7 +5,7 @@
 var=$(pwd) # finds wokring directory
 
 # starts open-stage-control and loads formuls-ui. This is available at 127.0.0.1:8080. GUI sends OSC messages on 8888, receives messages on 8889
-./open-stage-control/open-stage-control.app/Contents/MacOS/open-stage-control -s 127.0.0.1:8888 -o 8889 -l ./open-stage-control/_main.json &
+./open-stage-control/open-stage-control.app/Contents/MacOS/open-stage-control --no-gui -s 127.0.0.1:8888 -o 8889 -l ./open-stage-control/_main.json &
 
 # starts "audio" instance of pd, loads _main-audio.pd patch
 ./formulsengine 1 $1 4 &
