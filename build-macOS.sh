fce35ea8a155c256095082ff72053715512437fe
James Dooley <info@formuls.co.uk>
2025-10-29 13:41:59 +0000

    Version upgrade
    Major UI change in o-s-c
    
    Signed-off-by: James Dooley <info@formuls.co.uk>

--- build-macOS.sh
+++ build-macOS.sh
@@ -6,7 +6,7 @@ mkdir build
 cp -r src/gui build/gui
 cp -r src/pd build/pd
 mkdir build/pd/externals
-cp src/python/formuls-0.2.5-beta.py build/formuls-0.2.5-beta.py
+cp src/python/formuls-0.2.6-beta.py build/formuls-0.2.6-beta.py
 cp src/python/pyinstaller.py build/pyinstaller.py
 
 # build faust pd externals
@@ -40,12 +40,12 @@ make
 cd ../build
 python3 pyinstaller.py
 
-cp -r icons dist/formuls-0.2.5-beta/_internal/icons
-cp -r pd dist/formuls-0.2.5-beta/_internal/pd
-cp -r gui dist/formuls-0.2.5-beta/_internal/gui
-cp -r libs dist/formuls-0.2.5-beta/_internal/libs
+cp -r icons dist/formuls-0.2.6-beta/_internal/icons
+cp -r pd dist/formuls-0.2.6-beta/_internal/pd
+cp -r gui dist/formuls-0.2.6-beta/_internal/gui
+cp -r libs dist/formuls-0.2.6-beta/_internal/libs
 
-cp -r dist/formuls-0.2.5-beta ../formuls-0.2.5-beta.app
+cp -r dist/formuls-0.2.6-beta ../formuls-0.2.6-beta.app
 
 # clean up build files
 cd ../
